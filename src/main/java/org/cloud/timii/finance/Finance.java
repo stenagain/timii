fee6b95fa065c1f5f529a683a95f61cb738c535f
Osten <reynardzefox@gmail.com>
2016-03-27 09:23:03 -0500

    Update for merge and introduced finance

---
+++ src/main/java/org/cloud/timii/finance/Finance.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package org.cloud.timii.finance;
+
+/**
+ * @author sten
+ *
+ */
+public interface Finance {
+	
+}
---
+++ src/main/java/org/cloud/timii/finance/FinancialResult.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package org.cloud.timii.finance;
+
+/**
+ * @author sten
+ *
+ */
+public class FinancialResult {
+	public enum ResultType {
+		APPROVED, DENIED;
+	}
+}
